5086a39f4ace01ae01ad2a34071fe88f7eb1feb5
jkh5039 <borm3403@naver.com>
2021-11-10 17:18:06 +0900

    Setting

---
+++ Assets/JKH/00.Scripts/JKH_UnitInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JKH_UnitInfo : MonoBehaviour
+{    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
