3bd17898f060881be60865a05d5eb1a58d3076e9
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-11-15 17:24:07 +0900

    TECH.json 갱신
    - technology.id 나무위키 참조

--- Assets/LJS/Scripts/TechnologyManager.cs
+++ Assets/LJS/Scripts/TechnologyManager.cs
@@ -6,9 +6,12 @@ using System.IO;
 
 public class TechnologyManager : Singleton<TechnologyManager>
 {
+    public bool isLoadJson;
+    public string fileName;
+
     public List<Technology> technologies;
 
-    public string fileName;
+
 
 
     // Start is called before the first frame update
@@ -17,13 +20,54 @@ public class TechnologyManager : Singleton<TechnologyManager>
 
         technologies = new List<Technology>();
 
-        // technologies.Add(new Technology(0, "Farm", 25, null, null));
-        // technologies.Add(new Technology(1, "Mine", 25, null, null));
-        // technologies.Add(new Technology(2, "Pasture", 25, null, null));
-        // ArrayDataSaveText<Technology>(technologies.ToArray(), fileName);
+        #region save
+        if(isLoadJson == false)
+        {
+            technologies.Add(new Technology(211, "Pottery", 25, null, null));
+            technologies.Add(new Technology(212, "Animal Husbandry", 25, null, null));
+            technologies.Add(new Technology(213, "Mining", 25, null, null));
 
-        technologies = new List<Technology>(ArrayDataLoadText<Technology>(fileName));
-        print(technologies);
+            technologies.Add(new Technology(223, "Irrigation", 50, null, new int[]{0}));
+            technologies.Add(new Technology(224, "Writing", 50, null, new int[]{0}));
+            technologies.Add(new Technology(225, "Archery", 50, null, new int[]{1}));
+
+            technologies.Add(new Technology(231, "Masonry", 80, null, null));
+            technologies.Add(new Technology(232, "Bronze Working", 80, null, null));
+            technologies.Add(new Technology(233, "Wheel", 80, null, null));
+
+            technologies.Add(new Technology(312, "Currency", 144, null, null));
+            technologies.Add(new Technology(313, "Horseback Riding", 144, null, null));
+            technologies.Add(new Technology(314, "Iron Working", 144, null, null));
+
+            technologies.Add(new Technology(322, "Mathematics", 240, null, null));
+            technologies.Add(new Technology(323, "Construction", 240, null, null));
+            technologies.Add(new Technology(324, "Engineering", 240, null, null));
+
+            technologies.Add(new Technology(411, "Military Tactics", 360, null, null));
+            technologies.Add(new Technology(412, "Apprenticeship", 360, null, null));
+            technologies.Add(new Technology(413, "Machinery", 360, null, null));
+
+            technologies.Add(new Technology(421, "Education", 468, null, null));
+            technologies.Add(new Technology(422, "Stirrups", 468, null, null));
+            technologies.Add(new Technology(423, "Military Engineering", 468, null, null));
+            technologies.Add(new Technology(424, "Castle", 468, null, null));
+
+            technologies.Add(new Technology(512, "Mass Production", 720, null, null));
+            technologies.Add(new Technology(513, "Banking", 720, null, null));
+            technologies.Add(new Technology(514, "Printing", 720, null, null));
+            technologies.Add(new Technology(515, "Gunpowder", 720, null, null));
+
+            ArrayDataSaveText<Technology>(technologies.ToArray(), fileName);
+        }
+        #endregion
+
+        #region load
+        if (isLoadJson)
+        {
+            // technologies = new List<Technology>(ArrayDataLoadText<Technology>(fileName));
+            // print(technologies);
+        }
+        #endregion
     }
 
     // Update is called once per frame
