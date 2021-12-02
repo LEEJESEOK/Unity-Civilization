54c4e0a6383ed08cf994bf103d7c2ca479bd3f1a
jkh5039 <borm3403@naver.com>
2021-12-02 17:08:37 +0900

    Get Func

--- Assets/JKH/00.Scripts/JKH_Move.cs
+++ Assets/JKH/00.Scripts/JKH_Move.cs
@@ -32,6 +32,9 @@ public class JKH_Move : MonoBehaviour
 
     public static JKH_Move instance;
 
+    //From RayMove
+    
+
     private void Awake()
     {
         if (instance == null)
@@ -81,6 +84,7 @@ public class JKH_Move : MonoBehaviour
                 print("원거리공격력: " + RangeAttack);
                 print("사거리: " + Range);
                
+                //유닛이있는 타일의 정보를 가져온다.
                 float radius = 0.05f;
                 Collider[] maps = Physics.OverlapSphere(transform.position, radius, ~layer);
                 if (maps.Length == 1)
@@ -132,9 +136,11 @@ public class JKH_Move : MonoBehaviour
                 {
                     print("맵맞추는듕");
                     //이러면서? 마우스가 맵 위에 올리면 1) 해당 좌표랑 V 2) 이동력 계산하기 X 하면 좋겠ㄴㅔ...
+                    //PathFinding 하기, 실시간 이동력 계산 
 
+                    //pathfinding> findPath().
 
-                    //print("마우스에 위치한 좌표" + hitInfo.transform.GetComponent<TerrainData>().x + ", " + hitInfo.transform.GetComponent<TerrainData>().y);
+                    print("마우스에 위치한 좌표" + hitInfo.transform.GetComponent<TerrainData>().x + ", " + hitInfo.transform.GetComponent<TerrainData>().y);
                     if (Input.GetButton("Fire1"))
                     {
                         endPosX = hitInfo.transform.GetComponent<TerrainData>().x;
@@ -230,12 +236,82 @@ public class JKH_Move : MonoBehaviour
 
 
 
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    public void AMove()
+    //Get "DrawRay & ray" Function from RayMove_Script ++++
+    public void checkNeighbour()
     {
-        //
+        RaycastHit hitInfo;
+        Quaternion originRotation = transform.rotation;
+
+
+        for (int i = 0; i < 6; i++)
+        {
+
+            //Vector3 tmp = new Vector3(2 * Mathf.Sin((60 * i) * (Mathf.PI / 180)), 
+            //    0, 2 * Mathf.Cos((60 * i) * (Mathf.PI / 180)));
+
+            //Vector3 dir = transform.position - tmp;
+            //dir.y = 0;
+            //print(dir);
+            //Ray ray = new Ray(transform.position, dir);
+            Quaternion rotation = originRotation * Quaternion.Euler(0, i * 60, 0);
+            var pos = transform.position;
+            pos.y = -.95f;
+            Ray ray = new Ray(pos, rotation * transform.forward);
+
+            //int layer = 1 << LayerMask.NameToLayer("TestCube");
+
+            if (Physics.Raycast(ray, out hitInfo, 1000))
+            {
+                print("쏜다?");
+                if (hitInfo.transform.gameObject)
+                {
+
+                    //해당 좌표를 가져온다.
+                    //Script xxx =GameObject.Find("xxx").GetComponent<Script>();
+                    if (hitInfo.transform.gameObject.tag == "Map")
+                    {
+                        print(i + "번째에서, " + hitInfo.transform.GetComponent<TerrainData>().x +
+                            ", " + hitInfo.transform.GetComponent<TerrainData>().y);
+
+                        //이웃의 이동력도 불러온다!!!
+                        print("이동력: " + hitInfo.transform.GetComponent<TerrainData>().output.movePower);
+                        //print("이웃타일의 이동력: "+ hitInfo.transform.GetComponent<TerrainData>().)
+
+                        #region ToDo
+                        //needs
+                        //시작지점의 좌표>> Move 스크립트에서, 버튼 눌렀을때 가져온다
+                        //목적지의 좌표>> 클릭된좌표가 목적지의 좌표가 된다. 
+                        //본인의 위치의 g(n)과 이웃의 g(n) (자신의 G + 이웃의 이동력  ) 계산
+                        //갈수있는 타일인가? BOOL (이미 왔던데, 벽 은 못움직이니까 제외)
+                        //----
+                        //g(n)= 시작지점부터 현위치까지 이동한거리
+                        //h(n)= 현위치에서 목적지까지의 거리
+                        //f(n)= g(n)+h(n)
+
+                        //이웃의 f(n) 점수를 합산해서 제일 낮은 위치로 이동
+                        //목적지에 도달할떄 까지 반복한다.
+                        //-----------
+                        //다시 되돌아가서 계산하는거 해야하는데 이거 어케함? 나중에하자
+                        #endregion
+
+                    }
+
+                }
+            }
+        }
     }
 
-    //Get From RayMove
+    public void DrawRay()
+    {
+        Quaternion originRotation = transform.rotation;
+        for(int i = 0; i < 6; i++)
+        {
+            Quaternion rotation = originRotation * Quaternion.Euler(0, i * 60, 0);
+            var pos = transform.position;
+            pos.y = -.95f; //잘 조절 해야함...
+            Ray ray = new Ray(pos, rotation * transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction * 15, Color.red);
+        }
+    }
 
 }
