00fb76c2b2ece9eea71c9ab8ac7e779e3e75daa7
hyojin-ahn <angela991014@naver.com>
2021-12-23 15:24:27 +0900

    temp

--- Assets/HYO/01Scripts/ColorManager.cs
+++ Assets/HYO/01Scripts/ColorManager.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorManager : MonoBehaviour
+public class ColorManager : Singleton<ColorManager>
 {
+    public List<Material> buildingMat;
+    public List<Material> unitMat;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
