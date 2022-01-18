7b909a7a3ad77716dfa2507f08264c4387415ea4
hyojin-ahn <angela991014@naver.com>
2022-01-18 14:46:22 +0900

    건물 prefab 수정

--- Assets/HYO/01Scripts/Building.cs
+++ Assets/HYO/01Scripts/Building.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Building : MonoBehaviour
+public class Building : CombatUnit
 {
     public int playerId;
+
     private void Awake()
     {
         playerId = GameManager.instance.currentPlayerId;
